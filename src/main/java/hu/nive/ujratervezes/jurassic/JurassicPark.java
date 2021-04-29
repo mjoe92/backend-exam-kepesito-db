3aa9d5c1c21f323115c3f845b8a83ed3cf3f3ac0
Jozsef Csurgai <56649112+mjoe92@users.noreply.github.com>
2021-04-29 11:54:04 +0200

    Done!

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,9 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -10,6 +14,25 @@ public class JurassicPark {
         this.dbUrl = dbUrl;
         this.dbUser = dbUser;
         this.dbPassword = dbPassword;
+    }
+
+    public List<String> checkOverpopulation() {
+        try (Connection conn = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
+            String sql = "SELECT *" +
+                    " FROM dinosaur" +
+                    " WHERE actual > expected" +
+                    " ORDER BY breed";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            List<String> breedList = new LinkedList<>();
+            while (rs.next()) {
+                String breed = rs.getString(1);
+                breedList.add(breed);
+            }
+            return breedList;
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error while adding new Author.", ex);
+        }
     }
 
 }
